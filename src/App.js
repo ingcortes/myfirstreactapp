d62f2ec2d20a76799185f62b648d5f52adfd49ef
ingcortes <adrian.cortes.10@gmail.com>
2021-04-10 15:34:15 -0500

    version1.0

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+class App extends Component {
+  constructor(props) {
+     super(props)
+     this.state = {
+        newTask: "",
+     }
+  }
+
+  handleTaskChange = (event) => {
+    this.setState({
+       newTask: event.target.value,
+    })
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <h1 className="title"><span aria-label="emoji" role="img">🔥</span>Hola Mundo <span aria-label="emoji" role="img">🔥</span></h1>
+        <input onChange={this.handleTaskChange} type="text" className="new-task"/>
+        <h2 className="test-label">{this.state.newTask}</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
